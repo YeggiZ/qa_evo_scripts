ad9c5cb4ecc268c75ddf0613c917a4fe912cd3a8
Yeggi Zaborowsky <yeggiz@OMFG---O.o>
2021-08-31 11:49:10 +0300

    2

--- create_edi.py
+++ create_edi.py
@@ -1,11 +1,25 @@
 #-*- coding: utf-8 -*-
 #!/usr/bin/python3
+import os
+import sys
 
-def main():
-    pass
+
+def log(text):
+    print(text)
+
+def get_schemas(name):
+    home_dir = os.getcwd()
+    available_schemas = os.listdir(f'{home_dir}/schemas')
+
+    if name.lower() in available_schemas:
+        log(f'The Shaema for document type "{name}" is in available schemas')
+    else:
+        log(f'The Shaema for document type "{name}" is in NOT available')
 
 
 if __name__ == "__main__":
-    pass
-
+    if len(sys.argv) > 1:
+        get_schemas(sys.argv[1])
+    else:
+        log('Empty request')
     
